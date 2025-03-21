05d6e83f995407ae101447d5d49cd6f066f20b37
Krillof <artem.krilof2002@mail.ru>
2025-03-22 00:18:34 +0500

    Last task

--- src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
+++ src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
@@ -2,18 +2,17 @@ package ru.kubsu.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
+import ru.kubsu.telegrambot.bot.MyAmazingBot;
 import ru.kubsu.telegrambot.controller.TestController;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
-	public static void main(String[] args) {
-		/*
-		TestController controller = new TestController();
-		String hello = controller.hello();
-		System.out.println(hello);
-		*/
 
+	public static void main(final String[] args) {
 		SpringApplication.run(TelegrambotApplication.class, args);
 	}
 
